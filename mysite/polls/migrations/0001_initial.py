367ce7740fa380ce52810e96d24e9dbac5c0bb06
akashsri99 <akashsri99@gmail.com>
2016-04-25 04:44:12 +0530

    .

---
+++ mysite/polls/migrations/0001_initial.py
@@ -0,0 +1,52 @@
+
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.db.models.deletion
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Choice',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('choice_text', models.CharField(max_length=100)),
+                ('votes', models.IntegerField(default=0)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('comment_text', models.CharField(max_length=100)),
+                ('name', models.CharField(max_length=50)),
+                ('posted_on', models.DateTimeField(default=django.utils.timezone.now)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Question',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('question_text', models.CharField(max_length=200)),
+                ('pub_date', models.DateTimeField(verbose_name='date published')),
+            ],
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='question',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.Question'),
+        ),
+        migrations.AddField(
+            model_name='choice',
+            name='question',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='polls.Question'),
+        ),
+    ]
---
+++ mysite/polls/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse ,HttpResponseRedirect
+#from django.template import loader  #no longer needed now
+from .models import Question, Choice
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from django.core.urlresolvers import reverse
+
+
+
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	context = {'latest_question_list': latest_question_list}
+	return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		posted_comment= question.objects.get(pk=request.POST.get['comment',False])
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	else:
+		posted_comment.save()
+	return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	return render(request, 'polls/results.html', {'question': question})
+
+
+def vote(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	try:
+		selected_choice = question.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, question.DoesNotExist):
+	# Redisplay the question voting form.
+		return render(request, 'polls/detail.html', {
+			'question': question,
+			'error_message': "You didn't select a choice.",
+		})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+ 		return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
